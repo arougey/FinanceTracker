09c23bc774b4f5df902a4c71a218e0e0f2e036c2
chloejepson <jepsoc@rpi.edu>
2023-11-26 09:56:35 -0500

    using tailwind css to streamline, adding styling

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,32 +1,60 @@
-import React from "react"
+import React from "react";
+import "tailwindcss/tailwind.css"; 
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Page1() {
   return (
-    <>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="First name"
-          aria-label="Last name"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="Expected pre-tax income"
-          aria-label="Expected income"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <InputGroup.Checkbox aria-label="Are your student loans deferred?" />
-        <Form.Control aria-label="Text input with checkbox" />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="If so, for how many years are they deferred?"
-          aria-label="Expected income"
-        />
-      </InputGroup>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-3/5">
+        <div className="mb-3">
+          <Form.Label htmlFor="first-name" className="block text-lg font-semibold mb-1">First Name</Form.Label>
+          <InputGroup>
+            <Form.Control
+              id="first-name"
+              placeholder="Enter first name here"
+              aria-label="First name"
+              className="border rounded py-2 px-4 w-full"
+            />
+          </InputGroup>
+        </div>
+        <div className="mb-3">
+          <Form.Label htmlFor="expected-income" className="block text-lg font-semibold mb-1">Expected Pre-tax Income</Form.Label>
+          <InputGroup>
+            <Form.Control
+              id="expected-income"
+              placeholder="Enter expected pre-tax income here"
+              aria-label="Expected income"
+              className="border rounded py-2 px-4 w-full"
+            />
+          </InputGroup>
+        </div>
+        <div className="mb-3">
+          <Form.Label htmlFor="deferred" className="block text-lg font-semibold mb-1">Are your student loans deffered?</Form.Label>
+          <InputGroup>
+            <Form.Select
+              aria-label="Select option"
+              className="border rounded py-2 px-4 w-full"
+              defaultValue=""
+            >
+              <option value="" disabled>Select option</option>
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </Form.Select>
+          </InputGroup>
+        </div>
+        <div className="mb-3">
+          <Form.Label htmlFor="deferred-years" className="block text-lg font-semibold mb-1">Years of Loan Deferral</Form.Label>
+          <InputGroup>
+            <Form.Control
+              id="deferred-years"
+              placeholder="If yes, How many years are they deffered?"
+              aria-label="Deferred years"
+              className="border rounded py-2 px-4 w-full"
+            />
+          </InputGroup>
+        </div>
+      </div>
+    </div>
   );
 }
